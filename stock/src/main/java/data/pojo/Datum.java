8f541a8c49a0834575d5e5a1a74dba530f8d2483
Albert Chan <ychen@meihuichina.com>
2017-05-19 18:56:21 +0800

    Add MA, EMA, MACD, signal, Histogram calculation functions

--- stock/src/main/java/data/pojo/Datum.java
+++ stock/src/main/java/data/pojo/Datum.java
@@ -1,18 +1,55 @@
 package data.pojo;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map.Entry;
 
 public class Datum {
-	private HashMap<String, Object> properties = new HashMap<>();
+	protected HashMap<String, Object> properties = new HashMap<>();
 
 	public Datum() {
 	}
 
 	public Datum(Datum datum, String[] props) {
+		for (String prop : props) {
+			build(prop, datum.get(prop));
+		}
+	}
+
+	public static Datum merge(List<? extends Datum> datumList, String[] props) throws RuntimeException {
+		if (datumList.size() == 0) {
+			throw new RuntimeException("empty datum list.");
+		}
+
 		Datum newDatum = new Datum();
 		for (String prop : props) {
-			newDatum.build(prop, datum.get(prop));
+			newDatum.build(prop, datumList.get(0).get(prop));
 		}
+		boolean allMatch = datumList.stream().allMatch(d -> {
+			for (String prop : props) {
+				if (!d.get(prop).equals(newDatum.get(prop))) {
+					return false;
+				}
+			}
+			return true;
+		});
+		if (!allMatch) {
+			throw new RuntimeException("datumList do not all match.");
+		}
+
+		HashSet<String> ignoredProperties = new HashSet<>(Arrays.asList(props));
+
+		for (Datum d : datumList) {
+			for (Entry<String, Object> e : d.getAllProperties().entrySet()) {
+				if (!ignoredProperties.contains(e.getKey())) {
+					newDatum.set(e.getKey(), e.getValue());
+				}
+			}
+		}
+
+		return newDatum;
 	}
 
 	public void set(String name, Object value) {
@@ -26,6 +63,10 @@ public class Datum {
 
 	public Object get(String name) {
 		return properties.get(name);
+	}
+
+	public HashMap<String, Object> getAllProperties() {
+		return properties;
 	}
 
 	public String toString() {
---
+++ stock/src/main/java/data/pojo/StockDatum.java
@@ -0,0 +1,29 @@
+package data.pojo;
+
+public class StockDatum extends Datum {
+	public static final String ID = "id";
+	public static final String TIME = "time";
+	public static final String[] KEY_PROPS = new String[] { ID, TIME };
+
+	public static final String H = "high";
+	public static final String L = "low";
+	public static final String O = "opening";
+	public static final String C = "closing";
+	public static final String MA = "MA";
+	public static final String EMA = "EMA";
+	public static final String MACD = "MACD";
+	public static final String SIGNAL = "Signal";
+	public static final String HISTOGRAM = "Histogram";
+
+	public StockDatum() {
+	}
+
+	public StockDatum(StockDatum datum, String[] props) {
+		super(datum, props);
+	}
+
+	public String[] getKeyProperties() {
+		return KEY_PROPS;
+	}
+
+}
