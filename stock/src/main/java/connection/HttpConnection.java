fa0584c65173a9e7b7fe3b31db8bc36308288681
Albert Chan <ychen@meihuichina.com>
2017-11-21 18:46:17 +0800

    1

--- stock/src/main/java/connection/HttpConnection.java
+++ stock/src/main/java/connection/HttpConnection.java
@@ -1,31 +1,22 @@
 package connection;
 
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 
 public class HttpConnection {
 
-	public static String getData(String url, String charset) throws Exception {
-		byte[] bytes = new byte[256];
-		InputStream in = null;
-		ByteArrayOutputStream out = new ByteArrayOutputStream();
-
-		try {
-			URL u = new URL(url);
-			in = u.openStream();
+	public static String getData(String url, String charset) throws IOException {
+		URL u = new URL(url);
+		try (InputStream in = u.openStream(); ByteArrayOutputStream out = new ByteArrayOutputStream();) {
 			int size;
+			byte[] bytes = new byte[256];
 			while ((size = in.read(bytes)) != -1) {
 				out.write(bytes, 0, size);
 			}
 			String result = out.toString(charset);
-
-			out.close();
 			return result;
-		} finally {
-			if (in != null) {
-				in.close();
-			}
 		}
 	}
 
